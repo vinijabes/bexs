6a6b87e479e1ec79778553998749829ffb6b609e
Vinícius Jabes <vinijabes@gmail.com>
2020-12-22 13:23:07 -0300

    fix: update repository to match new graph

--- infrastructure/repository/file/path_repository.go
+++ infrastructure/repository/file/path_repository.go
@@ -6,7 +6,6 @@ import (
 	"encoding/csv"
 	"fmt"
 	"io"
-	"log"
 	"os"
 	"strconv"
 	"strings"
@@ -17,16 +16,14 @@ type filePathRepository struct {
 	file     *os.File
 }
 
-func (fpr *filePathRepository) GetGraph() model.Graph {
+func (fpr *filePathRepository) GetGraph() (model.Graph, error) {
 	file, err := os.Open(fpr.filepath)
 	if err != nil {
-		log.Fatal(err)
+		return model.Graph{}, err
 	}
 
 	r := csv.NewReader(file)
-
-	var vertexes []model.Vertex = []model.Vertex{}
-	var vertexesReference map[model.VertexID]int = make(map[model.VertexID]int)
+	graph := model.NewGraph()
 
 	for {
 		record, err := r.Read()
@@ -35,54 +32,34 @@ func (fpr *filePathRepository) GetGraph() model.Graph {
 		}
 
 		if err != nil {
-			log.Fatal(err)
+			return model.Graph{}, err
 		}
 
 		price, err := strconv.ParseUint(record[2], 10, 64)
 		if err != nil {
-			log.Fatal(err)
+			return model.Graph{}, err
 		}
 
 		origin := model.VertexID(record[0])
 		dest := model.VertexID(record[1])
 
-		if _, exists := vertexesReference[origin]; !exists {
-			vertexes = append(vertexes, model.Vertex{
-				ID: model.VertexID(origin),
-			})
-
-			vertexesReference[origin] = len(vertexes) - 1
-		}
-
-		if _, exists := vertexesReference[dest]; !exists {
-			vertexes = append(vertexes, model.Vertex{
-				ID: model.VertexID(dest),
-			})
-
-			vertexesReference[dest] = len(vertexes) - 1
-		}
-
-		originVertexIndex := vertexesReference[origin]
-
-		edge := model.Edge{
-			Origin: originVertexIndex,
-			Dest:   vertexesReference[dest],
+		graph.AddRoute(model.Route{
+			Origin: origin,
+			Dest:   dest,
 			Price:  price,
-		}
-
-		vertexes[originVertexIndex].Edges = append(vertexes[originVertexIndex].Edges, edge)
+		})
 	}
 
-	return model.Graph{Vertexes: vertexes, VertexReference: vertexesReference}
+	return *graph, nil
 }
 
-func (fpr *filePathRepository) AddEdge(edge model.Edge) error {
+func (fpr *filePathRepository) AddRoute(route model.Route) error {
 	file, err := os.OpenFile(fpr.filepath, os.O_APPEND|os.O_WRONLY, os.ModeAppend)
 	if err != nil {
 		return err
 	}
 
-	_, err = file.WriteString(fmt.Sprintf("\n%s", strings.Join([]string{string(edge.Origin), string(edge.Dest), strconv.FormatUint(edge.Price, 10)}, ",")))
+	_, err = file.WriteString(fmt.Sprintf("\n%s", strings.Join([]string{string(route.Origin), string(route.Dest), strconv.FormatUint(route.Price, 10)}, ",")))
 	return err
 }
 
